e7f36fc7f61252d0836064e9d355cd61d985946c
anuraj01 <anuraj01@gmail.com>
2022-01-04 22:20:13 +0530

    Update LeetCode_1_TwoSum.js
    Use obj / Map alternative to not to use indexOf

--- LeetCode_1_TwoSum.js
+++ LeetCode_1_TwoSum.js
@@ -22,5 +22,33 @@ var twoSum = function(nums, target) {
     return result;
 };
 
+// using plain object to replace IndexOf, so that internal Iteration can be avoided
+var twoSum_usingObj = function(nums, target) {
+    let indexFinder = {};
+    for(let count = 0; count < nums.length; count ++) {
+        let numToBeAdded = target - nums[count];
+        if (typeof indexFinder[numToBeAdded] !== 'undefined') {
+            return [count, indexFinder[numToBeAdded]]
+        }
+        indexFinder[nums[count]] = count;
+    }
+    
+    return [0, 1];
+};
+
+// using map to replace IndexOf, so that internal Iteration can be avoided
+var twoSum_usingMap = function(nums, target) {
+    let indexFinder = new Map();
+    for(let count = 0; count < nums.length; count ++) {
+        let numToBeAdded = target - nums[count];
+        if (indexFinder.has(numToBeAdded)) {
+            return [count, indexFinder.get(numToBeAdded)]
+        }
+        indexFinder.set(nums[count], count);
+    }
+    
+    return [0, 1];
+};
+
 
 twoSum([2,7,11,15], 9)
