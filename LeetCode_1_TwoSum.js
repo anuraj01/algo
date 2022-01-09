8ba0b0b0d2a1e250307b6c8a232d341df74f8d05
anuraj01 <anuraj01@gmail.com>
2022-01-09 15:06:12 +0530

    Update LeetCode_1_TwoSum.js
    Explanation and question added

--- LeetCode_1_TwoSum.js
+++ LeetCode_1_TwoSum.js
@@ -1,8 +1,33 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+    
+    Example 1: 
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    
+    Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
  */
+
+
+/**
+ * Step 1 : Iterate over the number array length
+ * Step 2 : Subtract the current number with target to identify which number to be added to get required target value
+ * Step 3 : Find indexof that required number and if index matches
+ *                       (or)
+ * Step 3 : Alternatively instead of finding index (as index itself is considered as one iteration), store the current number in a map or an object
+ * Step 4 : on next iteration, find req number to be added from step 2 and lookup from the object or map (Lookup is much more faster than indexOf)
+ * Step 5 : If found, return the array of count and lookup number
+ *
+ *
+ */
+
 var twoSum = function(nums, target) {
     let result = [];
     for (let count = 0; count < nums.length; count ++) {
