6acfa18f430ebe3924ff33cd551c7a0eac5e1d26
anuraj01 <anuraj01@gmail.com>
2022-01-09 15:13:31 +0530

    Update LeetCode_3_LongestSubstringWithoutRepeatingCharacter.js

--- LeetCode_3_LongestSubstringWithoutRepeatingCharacter.js
+++ LeetCode_3_LongestSubstringWithoutRepeatingCharacter.js
@@ -1,6 +1,27 @@
 /**
- * @param {string} s
- * @return {number}
+    Given a string s, find the length of the longest substring without repeating characters.
+    
+    Example 1:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+    
+    Example 2: 
+    Input: s = "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1
+    
+ */
+
+/**
+ * TIP : For loop may not be suitable, as we need to traverse again to previous position to identify next unique characters, so use While loop
+ * Step 1 : Have a unique array
+ * Step 2 : Add a characters one by one until we get a duplicate in that unique array
+ * Step 3 : Find max length always when we add value to the unique array
+ * Step 4 : If duplicate found, remove characters from begenning of unique array until we remove that duplicate entry from first occurence
+ * Step 5 : Do not increment index when removing item from first position from step 4
+ * Step 6 : As max length is always max of current max and unique length, after all iteration, max will have the result we needed
+ *
  */
 var lengthOfLongestSubstring = function(s) {
     if (s.length === 0 || s.length === 1) {
