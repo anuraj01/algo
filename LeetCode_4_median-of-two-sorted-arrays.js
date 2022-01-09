12d78bef7bf9e84a68c06b1f6a0e5116f25512be
anuraj01 <anuraj01@gmail.com>
2022-01-09 15:17:38 +0530

    Update LeetCode_4_median-of-two-sorted-arrays.js

--- LeetCode_4_median-of-two-sorted-arrays.js
+++ LeetCode_4_median-of-two-sorted-arrays.js
@@ -1,7 +1,24 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
+  Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+  
+  Example 1:
+  Input: nums1 = [1,3], nums2 = [2]
+  Output: 2.00000
+  Explanation: merged array = [1,2,3] and median is 2.
+  
+  Example 2:
+  Input: nums1 = [1,2], nums2 = [3,4]
+  Output: 2.50000
+  Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+  
+ */
+
+/**
+ * Step 1 : Concat both arrays
+ * Step 2 : Sort it in ascending order again, to make sure the concat array is sorted
+ * Step 3 : If length of concat array is odd, return mid element / 2
+ * Step 4 : If length is even, return center 2 elements and dividedby 2
+ *
  */
 var findMedianSortedArrays = function(nums1, nums2) {
   
