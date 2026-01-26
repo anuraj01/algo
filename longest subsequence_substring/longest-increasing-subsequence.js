57212bb0192ae271992795a4c03adfd05fdaa305
anuraj01 <anuraj01@gmail.com>
2026-01-26 06:33:59 +0530

    Update longest-increasing-subsequence.js

--- longest subsequence_substring/longest-increasing-subsequence.js
+++ longest subsequence_substring/longest-increasing-subsequence.js
@@ -60,3 +60,27 @@ lengthOfLIS([10,9,2,5,3,7,101,18]); // 4
     
     1   1   1   2   2   3    4    4
 **/
+// TS
+function lengthOfLIS(nums: number[]): number {
+  const n = nums.length;
+  const dp: number[] = new Array(n).fill(1);
+
+  let maxLen = 1;
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+    maxLen = Math.max(maxLen, dp[i]);
+  }
+
+  return maxLen;
+}
+
+/**
+    Complexity
+        Time: O(n²) → n ≤ 2500 is fine
+        Space: O(n)
+**/
