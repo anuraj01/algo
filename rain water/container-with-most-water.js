5aaafbb0d8fb16b07980c4710a691bd16c986589
anuraj01 <anuraj01@gmail.com>
2026-01-29 22:07:42 +0530

    Update container-with-most-water.js

--- rain water/container-with-most-water.js
+++ rain water/container-with-most-water.js
@@ -1,4 +1,6 @@
 /*
+https://leetcode.com/problems/container-with-most-water/description/
+
 You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
@@ -50,3 +52,49 @@ var maxArea = function(height) {
     
     return max_area;
 };
+
+
+/**
+ TS based
+
+ Key point
+  - The area is always limited by the shorter of the two lines.
+  - Therefore, moving the taller line cannot improve the area, because the shorter line still limits it.
+  
+ Time Complexity: O(n)
+    - Each pointer moves inward at most n times
+    - Single pass through the array
+Space Complexity: O(1)
+    - Constant extra space
+    - No auxiliary data structures
+
+**/
+function maxArea(height: readonly number[]): number {
+    if (height.length < 2) {
+        return 0;
+    }
+
+    let left: number = 0;
+    let right: number = height.length - 1;
+
+    let maxArea: number = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const minHeight = Math.min(height[left], height[right]);
+        const currentArea = minHeight * width;
+
+        if (currentArea > maxArea) {
+            maxArea = currentArea;
+        }
+
+        // Move the pointer limiting the area
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+}
